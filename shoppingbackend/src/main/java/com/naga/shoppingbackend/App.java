0152834f530eff299097de428b172d79e79f2612
krishna <vnagarajan541@gmail.com>
2019-08-23 10:56:23 +0530

    My project setup completed successfully

---
+++ shoppingbackend/src/main/java/com/naga/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.naga.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
